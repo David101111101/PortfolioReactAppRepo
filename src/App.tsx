c43acfea76fe2e816d860bbebc4a51a16b1cb116
Dave <davidstevenabril@gmail.com>
2026-04-12 00:08:37 -0500

    fix: correct case sensitivity in Dashboard import and route path

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Dashboard from "./pages/dashboard";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
     <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/Dashboard" element={<Dashboard />} />
       </Routes>
     </HashRouter>
   );
